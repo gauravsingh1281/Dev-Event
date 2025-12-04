4639f9b5acf28bc69f6679ad7ec0642609706b97
Gaurav Pratap Singh <gs492979@gmail.com>
2025-12-04 19:16:52 +0530

    header section and event card created with initial setup.

---
+++ components/EventCard.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  title: string;
+  image: string;
+}
+
+const EventCard = ({ title, image }: Props) => {
+  return (
+    <Link href={"/events"} id="event-card">
+      <Image
+        src={image}
+        alt={title}
+        width={410}
+        height={300}
+        className="poster"
+      />
+    </Link>
+  );
+};
+
+export default EventCard;
